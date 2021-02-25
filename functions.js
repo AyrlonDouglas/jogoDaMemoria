2921c61e119281252797956b3ac4a1a7215c1507
AyrlonDouglas <ayr.l@hotmail.com>
2021-02-24 23:09:26 -0300

    implementado logica, virada das cartas

--- functions.js
+++ functions.js
@@ -30,33 +30,81 @@ module.exports = {
 
     // render
 
-    montarTabela: function montarTabela(linhas, colunas, html, imagensPraTabela, arrayFundo, index) {
+    montarTabela: function montarTabela(linhas, colunas, html, imagensPraTabela, arrayFundo, query) {
         let gameHTML = html;
         let indexImagensTabela = 0;
-        let planoPreto = 'https://i.pinimg.com/736x/d6/39/2e/d6392eb6376d3b147fea9933996ddb4a.jpg';
+        let planoFundo = "https://i0.wp.com/www.multarte.com.br/wp-content/uploads/2019/01/free-flyers-background-png.png?resize=696%2C696&ssl=1"
 
-        gameHTML += '<table>';
+        for (let i = 0; i < arrayFundo.length; i++) {
+            for (let j = 0; j < arrayFundo.length; j++) {
+                if (arrayFundo[i] == 1 && arrayFundo[j] == 1 && i != j) {
+                    if (imagensPraTabela[i]['nome'] == imagensPraTabela[j]['nome']) {
+                        arrayFundo[i] = 2;
+                        arrayFundo[j] = 2
+                    }
+                }
+            }
+        }
+
+        gameHTML += '<div class="tabela-jogo"><table>';
 
         for (let i = 0; i < linhas; i++) {
-            gameHTML += '<tr>';
-            for (let j = 0; j < colunas; j++) {
-                if (arrayFundo[index] == 0) {
-                    gameHTML += `<td >
-                                    <a href='/gaming?index=${indexImagensTabela}&lin=${i}&col=${j}'><img class="hidden" src="${planoPreto}"></a>
-                                </td>`
-                    indexImagensTabela++
 
-                } else {
-                    gameHTML += `<td>
-                                    <a href='/gaming?index=${indexImagensTabela}&lin=${i}&col=${j}'><img src="${imagensPraTabela[indexImagensTabela]['url']}"></a>
-                                </td>`
+            gameHTML += '<tr>';
+
+            for (let j = 0; j < colunas; j++) {
+                if (arrayFundo[indexImagensTabela] == 1) {
+
+                    gameHTML += `<td><img src="${imagensPraTabela[indexImagensTabela]['url']}"></td>`
                     indexImagensTabela++
                 }
+                else if (arrayFundo[indexImagensTabela] == 2) {
 
+                    gameHTML += `<td><img src="${imagensPraTabela[indexImagensTabela]['url']}"></td>`
+                    indexImagensTabela++
+
+                }
+                else {
+                    gameHTML += `<td ><a href='/gaming?index=${indexImagensTabela}&lin=${i}&col=${j}'><img src='${planoFundo}' alt='FUNDO'></a></td>`
+                    indexImagensTabela++
+                }
             }
             gameHTML += '</tr>'
         }
-        gameHTML += '</table>'
+        gameHTML += '</table></div>'
+
         return gameHTML
     },
+
+    zerarArrayFundo: function zerarArrayFundo(array) {
+        let count1 = 0;
+        let zerado = false;
+
+
+        for (let i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                count1++;
+            }
+
+            if (count1 > 2) {
+                zerado = true
+                for (let j = 0; j < array.length; j++) {
+                    if (array[j] == 1) {
+                        array[j] = 0;
+                    }
+                }
+            }
+        } return zerado
+    },
+    vencedor: function vencedor(arr) {
+        let venceu = true;
+        for (let i = 0; i < arr.length; i++) {
+            if (arr[i] != 2) {
+                venceu = false;
+            }
+        }
+        if (venceu) {
+            console.log('VENCEU')
+        }
+    }
 }
--- server.js
+++ server.js
@@ -5,9 +5,33 @@ const func = require('./functions')
 const app = express();
 const port = 9000;
 
+let layoutBase = `<!DOCTYPE html>
+                    <html lang="en">
+
+                    <head>
+                        <meta charset="UTF-8">
+                        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                        <link rel="stylesheet" href="style.css">
+                        <title>Memory Game</title> 
+                    </head>
+
+                    <body>
+                        <header>
+                            <h1>Memory Game</h1>
+                            <nav>
+                                <ul>
+                                    <li><a href="/">Start</a></li>
+                                    <li><a href="#">Help</a></li>
+                                    <li><a href="/authors">Authors</a></li>
+                                </ul>
+                            </nav>
+                        </header>`;
+let queryVariavels;
 let gameHTML;
 let arrayFundo;
 let imagensPraTabela;
+let contadorClicks;
 let imagens = [
     {
         'nome': 'css',
@@ -43,6 +67,21 @@ let imagens = [
         'nome': 'html',
         'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/61/HTML5_logo_and_wordmark.svg/1200px-HTML5_logo_and_wordmark.svg.png',
         'used': 0,
+    },
+    {
+        'nome': 'python',
+        'url': 'https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67',
+        'used': 0,
+    },
+    {
+        'nome': 'vue',
+        'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/1200px-Vue.js_Logo_2.svg.png',
+        'used': 0,
+    },
+    {
+        'nome': 'angular',
+        'url': 'https://img2.gratispng.com/20181108/glf/kisspng-angularjs-in-action-javascript-application-softwar-angular-products-just-stickers-just-stickers-5be4f7a1467125.6693886615417322572885.jpg',
+        'used': 0,
     }
 ]
 let linhas = 2;
@@ -67,12 +106,13 @@ app.get("/", function (req, res) {
                                 <ul>
                                     <li><a href="/">Start</a></li>
                                     <li><a href="#">Help</a></li>
+                                    <li><a href="/authors">Authors</a></li>
                                 </ul>
                             </nav>
                         </header>
                         <main>
                             <h2>Have fun with the best DemoDay game!</h2>
-                            <p><a class="btn-grad" href="/game">Let's play!</a></p>
+                            <p><a class="btn-grad" href="/level">Let's play!</a></p>
                     
                         </main>
                     
@@ -81,9 +121,25 @@ app.get("/", function (req, res) {
                     </html>`
     res.send(inicioHtml)
 });
+app.get("/authors", (req, res) => {
+    let html = layoutBase;
+    html += `
+    <ul class="authors">
+        <li>Ayrlon Vilarim</li>
+        <li>Gustavo Cesar</li>
+        <li>Flávio Cesar</li>
+        <li>Henrique R.</li>
+        <li>Herica Suassuna</li>
+    </ul>
+    `
+    res.send(html)
+})
 
 app.get("/game", function (req, res) {
-
+    contadorClicks = 0;
+    queryVariavels = req.query
+    linhas = req.query.linLevel;
+    colunas = req.query.colLevel;
 
     imagensPraTabela = [];
 
@@ -92,8 +148,6 @@ app.get("/game", function (req, res) {
     arrayFundo = [];
 
     let urlImagemTransparencia = 'https://i1.wp.com/www.deviante.com.br/wp-content/uploads/2018/11/HelloWorld.jpg?fit=1000%2C350'
-
-
 
     gameHTML = `<!DOCTYPE html>
                         <html lang="en">
@@ -113,13 +167,11 @@ app.get("/game", function (req, res) {
                                     <ul>
                                         <li><a href="/">Start</a></li>
                                         <li><a href="#">Help</a></li>
+                                        <li><a href="/authors">Authors</a></li>
                                     </ul>
                                 </nav>
                             </header>
-                            <div class="button">
-                                <button type="button">Reset</button>
-                            </div>
-                            <div class="tabela-jogo">`
+                             <div class="tabela-jogo">`
 
     func.embaralharArray(imagens);
 
@@ -129,29 +181,51 @@ app.get("/game", function (req, res) {
 
     func.criarArrayTransparencia((linhas * colunas), arrayFundo)
 
-    gameHTML = func.montarTabela(linhas, colunas, gameHTML, imagensPraTabela, arrayFundo);
+    gameHTML = func.montarTabela(linhas, colunas, gameHTML, imagensPraTabela, arrayFundo, queryVariavels);
 
     gameHTML += '</div></body></html>';
+    //let gamingHTML = gameHTML
     // console.log(req.query)
     res.send(gameHTML)
 });
+app.get('/level', (req, res) => {
+
+    html = layoutBase;
+    html += `<ul>
+                <a href="/game?linLevel=2&colLevel=4"><li class="btn-grad level levelEasy">Easy</li></a>
+                <a href="/game?linLevel=2&colLevel=6"><li class="btn-grad level levelAverage">Average</li></a>
+                <a href="/game?linLevel=4&colLevel=5"><li class="btn-grad level">Hard</li></a>
+            </ul> `
+    html += `</body></html>`
+    res.send(html)
+})
 
 
 app.get("/gaming", (req, res) => {
-    // let query = req.query.index
 
-    // if (arrayFundo[req.query.index] == 0) {
-    //     arrayFundo[req.query.index] = 1;
+    contadorClicks++
 
-    // } else if (arrayFundo[req.query.index] == 1) {
-    //     arrayFundo[req.query.index] = 0;
-    // }
+    if (arrayFundo[req.query.index] == 0) {
+        arrayFundo[req.query.index] = 1;
+    }
+
+    let gamingHTML = layoutBase;
+    gamingHTML += `<p>Clicks :${contadorClicks}</p>`
+    queryVariavels = req.query;
+
+    let zerarArray = func.zerarArrayFundo(arrayFundo)
 
 
+    let renderGamingHTML = func.montarTabela(linhas, colunas, gamingHTML, imagensPraTabela, arrayFundo, queryVariavels);
 
-    console.log(req.query)
+
+    func.vencedor(arrayFundo)
+    //console.log(req.query)
     console.log(arrayFundo)
-    res.send(gameHTML)
+    // if (zerarArray) {
+    //     res.redirect('/gaming')
+    // }
+    res.send(renderGamingHTML)
 })
 
 app.use(express.static('public'));
